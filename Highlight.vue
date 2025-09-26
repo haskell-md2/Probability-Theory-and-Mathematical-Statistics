348cbd3c50ab2ae25f37b1c2fdd88fdf6a5b44c7
haskell_md2 <mike@DESKTOP-FASOEPM.localdomain>
2025-09-26 13:34:58 +0300

    demo v2 (lec3)

---
+++ Highlight.vue
@@ -0,0 +1,13 @@
+  <template>
+    <span class="highlight">
+      <slot></slot>
+    </span>
+  </template>
+
+  <style scoped>
+  .highlight {
+    background-color: yellow;
+    padding: 2px 4px;
+    border-radius: 3px;
+  }
+  </style>
---
+++ test.md
@@ -0,0 +1,5 @@
+Тестовый текст!
+
+<Highlight>sdf</Highlight>
+
+<b>Theorem 1.1</b> (Pythagoras) <i>The sum of the squares on the legs of a right triangle equals the square on the hypotenuse.</i>
